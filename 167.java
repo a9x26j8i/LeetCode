b566a560191d8821bd0091f44ce4101d1608e649
a9x26j8i <zewei_liu@outlook.com>
2018-06-28 01:36:25 +0800

    Update 167.java

--- 167.java
+++ 167.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        //init the array dynamically(using new instead of {})
+        //init the array dynamically(not using {})
         //two points to narrow down the possible combination(s)
         int[] res = new int[2];
         int i = 0, j = numbers.length-1, sum=0;
