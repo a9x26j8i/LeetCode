e718dffb35062f090ac0c4878ef253b5427b50a7
a9x26j8i <zewei_liu@outlook.com>
2018-06-28 01:28:18 +0800

    2 pointers to narrow down, O(n) not O(n^2)

---
+++ 167.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        //init the array dynamically(using new instead of {})
+        //two points to narrow down the possible combination(s)
+        int[] res = new int[2];
+        int i = 0, j = numbers.length-1, sum=0;
+        while(i<j){
+            sum = numbers[i] + numbers[j];
+            if(sum>target)
+                j--;
+            else if(sum<target)
+                i++;
+            else{
+                res[0] = ++i;
+                res[1] = ++j;
+                break;
+            }
+        }
+        return res;
+    }
+}
