1391ca3ab0972f94bcb4b66d93e174dc9515e03d
a9x26j8i <zewei_liu@outlook.com>
2018-07-03 00:03:53 +0800

    Update 268.java

--- 268.java
+++ 268.java
@@ -1,6 +1,7 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int result = nums.length;
+        //using xor between indices and the values.
         for(int i=0; i<nums.length; i++){
             result = result ^ i ^ nums[i];
         }
