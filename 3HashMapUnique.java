933148e16ec850f810eb52ddc10ac1fbbc33a35a
a9x26j8i <zewei_liu@outlook.com>
2018-07-06 23:49:03 +0800

    HashMap Entry-put when verifying

---
+++ 3HashMapUnique.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int max=0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for(int i=0, j=0; j<s.length(); j++){
+            if(map.containsKey(s.charAt(j))){
+                i = Math.max(map.get(s.charAt(j))+1, i);
+            }
+            max = Math.max(max, j-i+1);
+            //validating while putting entries into HashMap
+            map.put(s.charAt(j), j);
+            if(j==s.length()-1)
+                max = Math.max(max, j-i+1);
+        }
+        return max;
+    }
+}
