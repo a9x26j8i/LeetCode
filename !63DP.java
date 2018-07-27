efd06b3ea9ab280dd8b0e5d4d75e0c00a484997a
a9x26j8i <zewei_liu@outlook.com>
2018-07-27 11:09:01 +0800

    Dynamic Programming

---
+++ !63DP.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int width = obstacleGrid[0].length ;
+        //dp itself is divisive(for left and above element)
+        //notations in dp is different with that in row[], the left most element in dp is always the newest data(for left),
+        //dp will traverse each row
+        int[] dp = new int[width];//initial condition: obstacleGrid[-1][i] == 0;
+        dp[0] = 1;
+        for(int[] row: obstacleGrid){
+            for(int i=0; i<width; i++){
+                if(row[i]==1) dp[i]=0;
+                else if(i>0){
+                    dp[i] += dp[i-1];
+                }
+            }
+        }
+        return dp[width-1];
+    }
+}
