2b1ae207e21de11e822a514123d7d3cfd0f9440d
a9x26j8i <zewei_liu@outlook.com>
2018-08-27 01:13:34 -0700

    NewtonBianrySqrt()

--- 69NewtonBinarySqrt().java
+++ 69NewtonBinarySqrt().java
@@ -1,4 +1,20 @@
-    long r = x;
-    while (r*r > x)
-        r = (r + x/r) / 2;
-    return (int) r;
+class Solution {
+	//epsilon(err) version.
+    public int mySqrt(int x) {
+        if(x==0) return 0;
+        double last, res = x;
+        double err = 1;
+        while(true){
+            last = res;
+            res = (double) (res + x/res)*0.5;
+            if(Math.abs(last-res)<err) break;
+        }
+        return (int)res;
+        
+//         long r = x;
+//     while (r*r > x)
+//         r = (r + x/r) / 2;
+//     return (int) r;
+    }
+    
+}
