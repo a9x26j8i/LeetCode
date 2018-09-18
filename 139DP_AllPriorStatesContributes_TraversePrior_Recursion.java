c84e8fc4d9547760938ec8858fecf05d7abe9efa
a9x26j8i <zewei_liu@outlook.com>
2018-09-17 22:25:46 -0700

    DP_TraversePriors'states_Recursion

---
+++ 139DP_AllPriorStatesContributes_TraversePrior_Recursion.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>();
+        for(String str : wordDict){
+            set.add(str);
+        }
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;
+        for(int i=1; i<=s.length(); i++){
+            for(int j=0; j<=i; j++){
+                if(dp[j]==true && set.contains(s.substring(j, i))){
+                    dp[i] = true;
+                }        
+            }
+        }
+        return dp[s.length()];
+        
+        
+         // return helper(s, 0, 0, set);
+    }
+    //Time Limit Exceed-Backtracking-withoutMemorization
+//     private boolean helper(String s, int start, int end, HashSet<String> map){
+//         if(start >= s.length()){
+//             return true;
+//         }
+//         if(end >= s.length() && start<s.length()){
+//             return false;
+//         }
+        
+//         boolean result = false;
+//         for(int i=end; i< s.length(); i++){
+//             if(map.containsKey(s.substring(start, i+1))){
+//                 result = result || helper(s, i+1, i+1, map);
+//             }
+//         }
+        
+//         return result;
+        
+//     }
+}
