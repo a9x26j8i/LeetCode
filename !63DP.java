9f0aedb93047f6551da48f9a0431824c7ad7c625
a9x26j8i <zewei_liu@outlook.com>
2018-09-30 00:00:45 -0700

    DP

--- !63DP.java
+++ !63DP.java
@@ -5,7 +5,7 @@ class Solution {
         //notations in dp is different with that in row[], the left most element in dp is always the newest data(for left),
         //dp will traverse each row
         int[] dp = new int[width];//initial condition: obstacleGrid[-1][i] == 0;
-        dp[0] = 1;
+        dp[0] = 1;  
         for(int[] row: obstacleGrid){
             for(int i=0; i<width; i++){
                 if(row[i]==1) dp[i]=0;
